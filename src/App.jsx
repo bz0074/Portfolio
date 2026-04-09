eecf3428e485ef23de88a14a04c021a2cde030e0
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-09 17:48:36 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import "./styles/App.css";
-
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Home />
-        <Projects />
-        <Contact />
-      </main>
-    </>
-  );
+  return <h1>IT WORKS</h1>;
 }
 
 export default App;
