5374e553be8e8bf23768b81d8721b3fac378f357
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-09 19:24:18 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Home />
       <Projects />
       <Contact />
+      <Resume />
     </div>
   );
 }
