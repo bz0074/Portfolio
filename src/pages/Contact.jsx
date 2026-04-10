7f9640a7d203b098ffbe4314ee057ae7ad852963
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-09 19:11:28 -0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,9 @@ function Contact() {
   return (
     <section style={{ padding: "60px" }}>
       <h2>Contact</h2>
-      <p>Email: example@email.com</p>
+      <p>Your name: What's your name</p>
+      <p>Your email: example@email.com</p>
+      <p>Your message: Enter your message</p>
     </section>
   );
 }
