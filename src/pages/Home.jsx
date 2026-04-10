d160c41af93c4ea78adf881fc4bc046694dbe798
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-09 19:51:17 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <section style={{ padding: "60px" }}>
+    <section id="home" style={{ padding: "60px" }}>
       <h1>Hi, I'm Baidaho Zanre</h1>
       <p>IT Graduate | Desktop Publisher | Aspiring IT Support & Systems Professional</p>
     </section>
