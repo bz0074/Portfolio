3c9fa9538fd3ec0c3ac5c2086ee3a606b1281645
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 07:46:24 -0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
       <h2>Contact</h2>
       <p>Email: example@email.com</p>
       <p>Phone: +1 (000) 000-0000</p>
-      <p>LinkedIn: linkedin.com/in/yourprofile</p>
+      <p>LinkedIn: www.linkedin.com/in/baidaho-z-871213159</p>
     </section>
   );
 }
