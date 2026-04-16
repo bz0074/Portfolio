e2e969f3de86ca75424b67e3c1dbfd83629be53e
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 05:17:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import "./styles/App.css";
 
 function App() {
   return (
